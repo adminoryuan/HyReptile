98a1904dbd7443f84bfb45ccecb65644594dcea6
yuan <1317332365@qq.com>
2022-04-15 16:46:45 +0800

    add RepitResponse

--- Example/Program.cs
+++ Example/Program.cs
@@ -53,10 +53,12 @@ namespace Example
 
             repitle.Accpect("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
 
-            repitle.AddCookie("JSESSIONID=A8EBF79CFFBE80C24EF6C91C7D3D46E8");
+            // repitle.AddCookie("JSESSIONID=A8EBF79CFFBE80C24EF6C91C7D3D46E8");
 
+            repitle.Headers("Accept-Encoding", "gzip, deflate");
             repitle.User_Agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36");
 
+            repitle.Headers("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8");
             repitle.Host("www.spvec.com.cn");
 
             repitle.Headers("Connection", "keep-alive");
